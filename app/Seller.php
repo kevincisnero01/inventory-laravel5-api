944ae2eec155989cb218005913a52cf40f03a274
kevincisnero01 <kevingabriel18@gmail.com>
2021-04-20 21:46:41 -0430

    Implementando relaciones

--- app/Seller.php
+++ app/Seller.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Product;
 
 class Seller extends User
 {
-    //
+    public function products()
+    {
+    	return $this->hasMany(Product::class);
+    }
 }
