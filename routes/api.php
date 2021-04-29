20f837a2c285de2b0cd016882bc37d8700f6a754
kevincisnero01 <kevingabriel18@gmail.com>
2021-04-28 22:11:33 -0430

    Implementando las Operaciones Complejas para Product part#1

--- routes/api.php
+++ routes/api.php
@@ -38,6 +38,9 @@ Route::resource('categories.transactions','Category\CategoryTransactionControlle
  **/
 
 Route::resource('products', 'Product\ProductController', ['only' => ['index','show']]);
+Route::resource('products.transactions', 'Product\ProductTransactionController', ['only' => ['index']]);
+Route::resource('products.buyers', 'Product\ProductBuyerController', ['only' => ['index']]);
+Route::resource('products.categories', 'Product\ProductCategoryController', ['only' => ['index','update','destroy']]);
 
 /**
  *	Transactions
